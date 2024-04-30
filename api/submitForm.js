14ad6178d788698bcff6684748ba90d89ac19d31
Chirag Parakh <cparakh53@gmail.com>
2024-04-30 14:55:58 +0530

    Your

---
+++ api/submitForm.js
@@ -0,0 +1,34 @@
+// submitForm.js
+
+const nodemailer = require('nodemailer');
+
+export default async function submitForm(req, res) {
+  if (req.method === 'POST') {
+    const { name, contact, email, eventDate, eventVenue, others } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'cparakh09@gmail.com',
+        pass: 'parakh#@09'
+      }
+    });
+
+    const mailOptions = {
+      from: 'cparakh09@gmail.com',
+      to: 'cparakh09@gmail.com',
+      subject: 'New client enquire',
+      text: `Name: ${name}\nContact: ${contact}\nEmail: ${email}\nEvent Date: ${eventDate}\nEvent Venue: ${eventVenue}\nOthers: ${others}`
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).send('Form submitted successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error sending email');
+    }
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
