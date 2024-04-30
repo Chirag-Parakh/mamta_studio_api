b2573919bbd649cb64b75426889ce98435bc3ed6
Chirag Parakh <cparakh53@gmail.com>
2024-04-30 16:41:05 +0530

    final

--- api/submitForm.js
+++ api/submitForm.js
@@ -27,7 +27,7 @@ export default async function submitForm(req, res) {
       const mailOptions = {
         from: 'cparakh53@gmail.com',
         to: 'rajdara.highway@gmail.com',
-        subject: 'New client enquire',
+        subject: 'New Client Enquiry',
         text: `Name: ${name}\nContact: ${contact}\nEmail: ${email}\nEvent Date: ${eventDate}\nEvent Venue: ${eventVenue}\nOthers: ${others}`
       };
 
