b1985b22bf54f249f981a83e645e0db743b06d99
Chirag Parakh <cparakh53@gmail.com>
2024-04-30 15:40:22 +0530

    final

--- api/submitForm.js
+++ api/submitForm.js
@@ -4,9 +4,9 @@ const nodemailer = require('nodemailer');
 
 
 export default async function submitForm(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization" );
   if(req.method === 'GET'){
      res.status(200).json({ message: 'Form submitted' });
   }
